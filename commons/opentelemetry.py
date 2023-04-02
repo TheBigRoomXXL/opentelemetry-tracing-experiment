48df284ad8769510b30e148bbe5dd5baaab7c552
TheBigRoomXXL <seb.lovergne@gmail.com>
2023-04-02 18:28:46 +0200

    fix: replace jaeger exporter (depreciated) with otel exporter

--- commons/opentelemetry.py
+++ commons/opentelemetry.py
@@ -1,7 +1,7 @@
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
@@ -12,12 +12,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 def register_opentelemetry(app: Flask, db: SQLAlchemy) -> None:
-    resource = Resource(attributes={SERVICE_NAME: "otel-tester"})
+    resource = Resource(attributes={SERVICE_NAME: "book-api"})
 
-    exporter = JaegerExporter(
-        agent_host_name="localhost",
-        agent_port=6831,
-    )
+    exporter = OTLPSpanExporter(endpoint="http://localhost:4317", insecure=True)
 
     processor = BatchSpanProcessor(exporter)
 
