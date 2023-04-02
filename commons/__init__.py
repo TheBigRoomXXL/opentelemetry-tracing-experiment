b4d1918c9f555ec6a7c218710abe6aaaf10cdd75
TheBigRoomXXL <seb.lovergne@gmail.com>
2023-04-02 18:05:21 +0200

    refacto: move api and db initialisation to dedicated files

--- commons/__init__.py
+++ commons/__init__.py
@@ -1,18 +1,2 @@
-from flask_smorest import Api
-from flask_sqlalchemy import SQLAlchemy
-from flask_sqlalchemy.model import Model
-
-
-class MyDbModel(Model):
-    """MyDbModel is used as the "model_class" of db (aka db.Model).
-    Extend the SQLAlchemy functionalities throught this class."""
-
-    def patch(self, update_dictionary: dict) -> None:
-        """Partial update an object with a simple and clear syntax"""
-        for col_name in self.__table__.columns.keys():
-            if col_name in update_dictionary:
-                setattr(self, col_name, update_dictionary[col_name])
-
-
-api = Api()
-db = SQLAlchemy(model_class=MyDbModel)
+from commons.smorest import api as api
+from commons.sqlalchemy import db as db
---
+++ commons/sqlalchemy.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy.model import Model
+
+
+class MyDbModel(Model):
+    """MyDbModel is used as the "model_class" of db (aka db.Model).
+    Extend the SQLAlchemy functionalities throught this class."""
+
+    def patch(self, update_dictionary: dict) -> None:
+        """Partial update an object with a simple and clear syntax"""
+        for col_name in self.__table__.columns.keys():
+            if col_name in update_dictionary:
+                setattr(self, col_name, update_dictionary[col_name])
+
+
+db = SQLAlchemy(model_class=MyDbModel)
